2346638862713762780f6b9ad5ea2489c2a2a18d
Ian MacLeod <ian@nevir.net>
2014-01-31 23:50:29 -0800

    1. Xcode Basics

---
+++ Resume/Resume/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Resume
+//
+//  Created by Ian MacLeod on 1/31/14.
+//  Copyright (c) 2014 Jane Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Resume/Resume/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Resume
+//
+//  Created by Ian MacLeod on 1/31/14.
+//  Copyright (c) 2014 Jane Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
