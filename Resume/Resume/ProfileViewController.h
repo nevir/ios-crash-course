0abf5834ddf020040afea4f4be34685139281cd2
Ian MacLeod <ian@nevir.net>
2014-02-01 08:23:46 -0800

    4. Extra Credit: Bouncing Background

--- Resume/Resume/ProfileViewController.h
+++ Resume/Resume/ProfileViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ProfileViewController : UIViewController
+@interface ProfileViewController : UIViewController <UIScrollViewDelegate>
 
 @end
--- Resume/Resume/ProfileViewController.m
+++ Resume/Resume/ProfileViewController.m
@@ -12,10 +12,19 @@
 
 @property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *backgroundImageView;
 
 @end
 
 @implementation ProfileViewController
+
+#pragma mark - View Lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.scrollView.delegate = self;
+}
 
 - (void)viewDidLayoutSubviews
 {
@@ -36,6 +45,26 @@
     }
 
     self.scrollView.contentSize = scrollContentSize;
+}
+
+#pragma mark - UIScrollViewDelegate
+
+// Advanced: Support for bouncing the background image when the user pulls down on the scroll view.
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    // We only want to extend above the scroll view.
+    CGFloat topOffset = MIN(self.scrollView.contentOffset.y, 0.0);
+
+    // Adjust the top and height so that the background is always pinned to the
+    // top of the screen (while the bottom stays pinned to the scrolling content).
+    CGRect frame = self.backgroundImageView.frame;
+    // Calculate the image's height as if we were at the top of the screen.
+    CGFloat baseHeight = frame.size.height + frame.origin.y;
+    // And then stretch us so that the bottom doesn't move.
+    frame.origin.y = topOffset;
+    frame.size.height = baseHeight - topOffset;
+
+    self.backgroundImageView.frame = frame;
 }
 
 @end
