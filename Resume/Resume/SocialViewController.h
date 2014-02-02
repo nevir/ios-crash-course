e9cb4c944d7457205646b0aab2a22a23c792bc4b
Ian MacLeod <ian@nevir.net>
2014-02-01 23:06:43 -0800

    8. Table Views

---
+++ Resume/Resume/SocialViewController.h
@@ -0,0 +1,13 @@
+//
+//  SocialViewController.h
+//  Resume
+//
+//  Created by Ian MacLeod on 2/1/14.
+//  Copyright (c) 2014 Jane Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialViewController : UITableViewController
+
+@end
